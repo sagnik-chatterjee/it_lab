b93af6f5891c9b9f65f4caa027f83c35db147e61
Sagnik <chatterjeesagnik88@gmail.com>
2021-05-30 16:04:48 +0530

    added files

---
+++ lab10/temp/frontend/src/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Heading } from "@chakra-ui/react"
+
+function Header() {
+    return (
+        <Heading size="lg" fontSize="50px">
+            I'm overriding this heading
+        </Heading>
+    )
+}
+export default Header;
